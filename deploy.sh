db2e92403911b9c7e71ca19b393e27bda8e3341b
Dylan Fontana <dyfontana@gmail.com>
2018-04-08 20:04:56 -0400

    Publishing to master (deploy.sh)

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out master branch into public"
+git worktree add public master
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
+hugo
+
+echo "Updating master branch"
+cd public && git add --all && git commit -m "Publishing to master (deploy.sh)" && git push
